9b30cf4b665c9921af99388440d875b1304d8407
Eric Briscoe <eric0briscoe@gmail.com>
2020-09-09 19:12:22 -0500

    fix the bug in break.py

---
+++ sec2/basic_if.py
@@ -0,0 +1,8 @@
+happy = True
+sad = False
+
+if happy:
+    print("I'm happy!")
+
+if sad:
+    print("I'm Sad!")
--- sec2/break.py
+++ sec2/break.py
@@ -1,13 +1,20 @@
 fruit = input("Guess which fruit I'm thinking of!: ")
 
-tries = 1
+tries = 2
 max_tries = 5
+winner = True
 
 while fruit != 'apple':
     fruit = input("Nope! Try again: ")
     
     if tries >= max_tries:
+        winner = False
         break
     tries += 1
 
-print("Nice! You guessed correctly!") # Something is wrong here!
+print(winner)
+if winner == False:
+    print("You Lose!")
+
+if winner == True:
+    print("You Win!")
