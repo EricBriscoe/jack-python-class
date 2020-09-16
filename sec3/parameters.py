3218c882dc2205c36d610142b47c2447f7d5dca3
Eric Briscoe <eric0briscoe@gmail.com>
2020-09-15 19:06:26 -0500

    sec3

---
+++ sec3/parameters.py
@@ -0,0 +1,13 @@
+def hello(name):
+    print('Hello, ' + name)
+
+
+hello('Jack')
+hello('Eric')
+
+
+# Define - explain/define what a function is to the computer to be remembered
+# Call
+# Pass
+# Argument
+# Parameter
