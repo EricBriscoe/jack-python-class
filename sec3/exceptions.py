51d0f20d9236a29a1c989f790513dd37a242a236
Eric Briscoe <eric0briscoe@gmail.com>
2020-09-15 19:50:00 -0500

    update

--- sec3/exceptions.py
+++ sec3/exceptions.py
@@ -1,10 +1,20 @@
-def division(numerator, denominator):
+def safeDivision(numerator, denominator):
     try:
         return numerator / denominator
     except ZeroDivisionError:
         return "Error! Cannot divide by zero!"
 
+def unsafeDivision(numerator, denominator):
+    return numerator / denominator
 
-print(division(4, 2))
-print(division(3, 2))
-print(division(2, 0))
+print(safeDivision(4, 2))
+print(safeDivision(2, 0))
+print(safeDivision(3, 2))
+
+
+
+
+print(unsafeDivision(4, 2))
+print(unsafeDivision(2, 0))
+print(unsafeDivision(3, 2))
+
