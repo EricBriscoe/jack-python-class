7810aec84849ebb177ab72cb5fa4fbc219f95f1b
Eric Briscoe <eric0briscoe@gmail.com>
2020-09-15 20:05:32 -0500

    update

---
+++ sec3/zigzag.py
@@ -0,0 +1,19 @@
+import time
+
+indent = 0
+output = '******'
+indentIncreasing = True
+
+while True:
+    print(' ' * indent, end='')
+    print(output)
+
+    if indentIncreasing:
+        indent += 1 # The same as indent = indent + 1
+        if indent > 20:
+            indentIncreasing = False
+    else: # indentDecreasing
+        indent += -1
+        
+
+    time.sleep(.1)
