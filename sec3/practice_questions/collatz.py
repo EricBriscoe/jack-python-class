9c154e19fb16efd801a391410c1a0bf89342634c
Eric Briscoe <eric0briscoe@gmail.com>
2020-09-17 19:18:13 -0500

    Update collatz.py

--- sec3/practice_questions/collatz.py
+++ sec3/practice_questions/collatz.py
@@ -2,6 +2,10 @@ def collatz(number):
     """
     Write a function which prints the collatz sequence
     per the specifications on pg. 76
+    
+    In summary you will create a loop until the number reaches 1
+    If the number is even your next number will be number//2
+    If the number is odd your next number will be 3 * number + 1
     """
     pass
 
