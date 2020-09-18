5ec0e18bf9b4ddbc8dc7f48fccfe89fed44c1abb
Eric Briscoe <eric0briscoe@gmail.com>
2020-09-17 19:52:50 -0500

    add to while.py

--- sec2/while.py
+++ sec2/while.py
@@ -4,3 +4,9 @@ while fruit != 'apple':
     fruit = input("Nope! Try again: ")
 
 print("Nice! You guessed correctly!")
+
+print("Counting up to 10")
+i = 0
+while i < 11:
+    print(i)
+    i = i + 1
