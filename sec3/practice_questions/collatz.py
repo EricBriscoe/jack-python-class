6c3844f71eb467957b28068e5bc3cac3fb1e6a10
Eric Briscoe <eric0briscoe@gmail.com>
2020-09-17 19:10:42 -0500

    Add collatz sequence prompt

---
+++ sec3/practice_questions/collatz.py
@@ -0,0 +1,11 @@
+def collatz(number):
+    """
+    Write a function which prints the collatz sequence
+    per the specifications on pg. 76
+    """
+    pass
+
+
+if __name__ == "__main__":
+    starting_num = input("Enter starting number")
+    collatz(starting_num)
