c71a5bb163c0827f3616428f8ec42be615b4edf8
Jack Briscoe <soccermanjb@icloud.com>
2020-09-23 19:47:11 -0400

    collatz finished

--- sec3/practice_questions/collatz.py
+++ sec3/practice_questions/collatz.py
@@ -7,13 +7,14 @@ def collatz(number):
     If the number is even your next number will be number//2
     If the number is odd your next number will be 3 * number + 1
     """
-    number = int
+    number = int(number)
     while number > 1:
         if number % 2 == 0:
             number = number // 2
-        if number % 2 == 1:
+            print(number)
+        elif number % 2 == 1:
             number = number * 3 + 1
-
+            print(number)
 
 
 if __name__ == "__main__":
