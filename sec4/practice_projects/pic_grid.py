f2c98b6ea2f9a7cfc97863e11549309356107cab
Eric Briscoe <eric0briscoe@gmail.com>
2020-10-14 19:35:29 -0500

    add pic grid print thing

--- sec4/practice_projects/pic_grid.py
+++ sec4/practice_projects/pic_grid.py
@@ -1,6 +1,26 @@
 def print_grid(outer_list):
-    pass
+    for i in range(len(outer_list[0])):
+        print(f"Working on column: {i}")
+        for inner_list in outer_list:
+            print(inner_list[i], end='')
+        print('')
+
+def print_zip_grid(outer_list):
+    for column in zip(*outer_list):
+        for char in column:
+            print(char, end='')
+        print('')
 
 if __name__ == "__main__":
-    test_case = []
-    print_grid(test_case)
+    test_case = [
+        ['.', '.', '.', '.', '.', '.'],
+        ['.', '0', '0', '.', '.', '.'],
+        ['0', '0', '0', '0', '.', '.'],
+        ['0', '0', '0', '0', '0', '.'],
+        ['.', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '.'],
+        ['0', '0', '0', '0', '.', '.'],
+        ['.', '0', '0', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.'],
+    ]
+    print_zip_grid(test_case)
